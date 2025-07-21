8d67f5397b73f235188aeffbcec931da024bc9a5
abhinav <abhinavsinghabhi56@gmail.com>
2025-07-21 08:05:44 +0530

    mask color fixed

--- src/components/DotBackground.jsx
+++ src/components/DotBackground.jsx
@@ -3,7 +3,7 @@ const DotBackground = ({ children }) => {
     <>
       <div className="bg-black w-screen min-h-screen relative">
         <div className="absolute inset-0 w-full h-full [background-image:radial-gradient(#404040_1px,transparent_1px)] [background-size:20px_20px]">
-          <div className="pointer-events-none absolute inset-0 bg-white dark:bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
+          <div className="pointer-events-none absolute inset-0 bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
         </div>
         <div className="relative z-50">{children}</div>
       </div>
